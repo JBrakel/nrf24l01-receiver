4d1618dafc45be58e3ece32c8f95d80fe06581ce
Jannis Brakel <jannis.brakel@tu-dortmund.de>
2025-12-05 21:09:46 +0100

    enable interrupts for nrf module

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #include "config.h"
 #include "spi.h"
@@ -12,7 +13,24 @@
 #include "nrf24l01.h"
 #include "shared.h"
 
+volatile uint8_t IS_NEW_DATA = 0;
+volatile  DualJoystickPacket joysticks;
+
+ISR(INT0_vect){
+  // Clear interrupt in NRF module
+  nrf_write_register(NRF_REG_STATUS, (1 << NRF_RX_DR));
+
+  // Receive Data
+  nrf_read_payload((uint8_t*)&joysticks, NRF_PAYLOAD_LENGTH);
+
+  // Toggle new data flag
+  IS_NEW_DATA = 1;
+}
+
 int main(){
+  // Disable global interrupts during setup
+  cli();
+
   // Init
   usart_init();
   switch_init();
@@ -22,103 +40,107 @@ int main(){
   spi_init();
   nrf_init();
   nrf_config_rx();
+
+  // Configure interrupt pin PD2 on falling edge
+  EICRA |= (1 << ISC01); 
+  EIMSK |= (1 << INT0);
+
+  // Enable global interrupts
+  sei();
   
   int16_t x_left, y_left, x_right, y_right;
   uint8_t sw_left, sw_right, pressed_left, pressed_right;
   char buffer[120];
-
-  uint8_t IS_ACTIVE_LEFT = 0;
-  uint8_t IS_ACTIVE_RIGHT = 0;
-
-  DualJoystickPacket joysticks;
   uint8_t threshold = 5;
+  
   while(1){
-    uint8_t status = nrf_read_register(NRF_REG_STATUS);
-    if (status & (1 << NRF_RX_DR)) {
+    if (IS_NEW_DATA) {
+
+      // Extract joystick data
+      x_left = joysticks.left.x;
+      y_left = joysticks.left.y;
+      sw_left = joysticks.left.button;
+      pressed_left = (sw_left == 0) ? 1 : 0;
+
+      x_right = joysticks.right.x;
+      y_right = joysticks.right.y;
+      sw_right = joysticks.right.button;
+      pressed_right = (sw_right == 0) ? 1 : 0;
+
+      // --------------------------------------------------------
+
+      // Convert for PWM
+      uint16_t x_left_pwm = abs(x_left) * 2;
+      uint16_t y_left_pwm = abs(y_left) * 2;
+      uint16_t x_right_pwm = abs(x_right) * 2;
+      uint16_t y_right_pwm = abs(y_right) * 2;
+      
+      if(x_left_pwm > 255)  x_left_pwm = 255;
+      if(y_left_pwm > 255)  y_left_pwm = 255;
+      if(x_right_pwm > 255) x_right_pwm = 255;
+      if(y_right_pwm > 255) y_right_pwm = 255;
+
+      // --------------------------------------------------------
+
+      // Use highest value 
+      uint16_t up_value = 0;
+      if (y_left > threshold || y_right > threshold) {
+          up_value = (y_left > y_right) ? y_left_pwm : y_right_pwm;
+      }
+
+      uint16_t down_value = 0;
+      if (y_left < -threshold || y_right < -threshold) {
+          down_value = (abs(y_left) > abs(y_right)) ? y_left_pwm : y_right_pwm;
+      }
+
+      uint16_t left_value = 0;
+      if (x_left < -threshold || x_right < -threshold) {
+          left_value = (abs(x_left) > abs(x_right)) ? x_left_pwm : x_right_pwm;
+      }
+
+      uint16_t right_value = 0;
+      if (x_left > threshold || x_right > threshold) {
+          right_value = (x_left > x_right) ? x_left_pwm : x_right_pwm;
+      }
+
+      // Clamp values to 255
+      if (up_value > 255) up_value = 255;
+      if (down_value > 255) down_value = 255;
+      if (left_value > 255) left_value = 255;
+      if (right_value > 255) right_value = 255;
+
+      // --------------------------------------------------------
+
+      // Evaluate buttons
+      if (pressed_left || pressed_right) {
+          pwm_set(LED_PIN_LEFT, 255);
+          pwm_set(LED_PIN_UP, 255);
+          pwm_set(LED_PIN_RIGHT, 255);
+          pwm_set(LED_PIN_DOWN, 255);
+      } 
+
+      // Idle case
+      else if (x_left == 0 && y_left == 0 && x_right == 0 && y_right == 0) {
+          pwm_set(LED_PIN_LEFT, 0);
+          pwm_set(LED_PIN_RIGHT, 0);
+          pwm_set(LED_PIN_UP, 0);
+          pwm_set(LED_PIN_DOWN, 0);
+      } 
+      
+      // Set LEDs 
+      else {
+          pwm_set(LED_PIN_UP, up_value);
+          pwm_set(LED_PIN_DOWN, down_value);
+          pwm_set(LED_PIN_LEFT, left_value);
+          pwm_set(LED_PIN_RIGHT, right_value);
+      }
         
-        // --------------------------------------------------------
+      // --------------------------------------------------------
 
-        // Receive Data
-        nrf_read_payload((uint8_t*)&joysticks, NRF_PAYLOAD_LENGTH);
-
-        x_left = joysticks.left.x;
-        y_left = joysticks.left.y;
-        sw_left = joysticks.left.button;
-        pressed_left = (sw_left == 0) ? 1 : 0;
-
-        x_right = joysticks.right.x;
-        y_right = joysticks.right.y;
-        sw_right = joysticks.right.button;
-        pressed_right = (sw_right == 0) ? 1 : 0;
-
-        // --------------------------------------------------------
-
-        // Convert for PWM
-        uint16_t x_left_pwm_value = abs(x_left) * 2;
-        uint16_t y_left_pwm_value = abs(y_left) * 2;
-        uint16_t x_right_pwm_value = abs(x_right) * 2;
-        uint16_t y_right_pwm_value = abs(y_right) * 2;
-        
-        if(x_left_pwm_value > 255)  x_left_pwm_value = 255;
-        if(y_left_pwm_value > 255)  y_left_pwm_value = 255;
-        if(x_right_pwm_value > 255) x_right_pwm_value = 255;
-        if(y_right_pwm_value > 255) y_right_pwm_value = 255;
-
-        // --------------------------------------------------------
-
-        // Reset LEDs in idle case
-        if (!pressed_left && !pressed_right && 
-            abs(x_left) <= threshold && abs(y_left) <= threshold 
-            && abs(x_right) <= threshold && abs(y_right) <= threshold){
-              pwm_set(LED_PIN_LEFT, 0);
-              pwm_set(LED_PIN_UP, 0);
-              pwm_set(LED_PIN_RIGHT, 0);
-              pwm_set(LED_PIN_DOWN, 0);
-
-              IS_ACTIVE_LEFT = 0;
-              IS_ACTIVE_RIGHT = 0;
-          }
-
-        // Evaluate buttons
-        else if (pressed_left || pressed_right) {
-            pwm_set(LED_PIN_LEFT, 255);
-            pwm_set(LED_PIN_UP, 255);
-            pwm_set(LED_PIN_RIGHT, 255);
-            pwm_set(LED_PIN_DOWN, 255);
-            IS_ACTIVE_LEFT = 0;  
-            IS_ACTIVE_RIGHT = 0;  
-        } 
-
-        // Evaluate analog joysticks
-        else{
-            // --------------------------------------------------------
-
-            // Left Joystick
-            if(!IS_ACTIVE_RIGHT){
-              // Horizontal control
-              pwm_control_axis(LED_PIN_RIGHT, LED_PIN_LEFT, x_left, x_left_pwm_value, threshold, &IS_ACTIVE_LEFT);
-              // Vertical control 
-              pwm_control_axis(LED_PIN_UP, LED_PIN_DOWN, y_left, y_left_pwm_value, threshold, &IS_ACTIVE_LEFT);
-            }
-            
-
-            // --------------------------------------------------------
-
-            // Right Joystick
-            if(!IS_ACTIVE_LEFT){
-            // Horizontal control 
-            pwm_control_axis(LED_PIN_RIGHT, LED_PIN_LEFT, x_right, x_right_pwm_value, threshold, &IS_ACTIVE_RIGHT);
-            // Vertical control 
-            pwm_control_axis(LED_PIN_UP, LED_PIN_DOWN, y_right, y_right_pwm_value, threshold, &IS_ACTIVE_RIGHT);
-          }
-        }
-        
-        // --------------------------------------------------------
-
-        // Printing
-        sprintf(buffer, "(%4d, %4d, %4d), (%4d, %4d, %4d)", x_left, y_left, pressed_left, x_right, y_right, pressed_right);
-        usart_tx(buffer);  
-        usart_tx("\n");
+      // Printing
+      sprintf(buffer, "(%4d, %4d, %4d), (%4d, %4d, %4d)", x_left, y_left, pressed_left, x_right, y_right, pressed_right);
+      usart_tx(buffer);  
+      usart_tx("\n");
     }
   }
 } 
